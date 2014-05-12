6ffb13d531f62a8cd4f8197c45d2a1a3e17e49f5
graup <paul@pgrau.de>
2014-05-12 12:38:21 +0900

    Restructured code, improved outputs

--- kakaotalk.py
+++ kakaotalk.py
@@ -4,14 +4,19 @@
 Simple analysis of KakaoTalk chat export files
 """
 
+from __future__ import division
+
 import sys, os
 import dateutil.parser
+from dateutil.relativedelta import relativedelta
 import datetime
 import re
 from collections import Counter
+import operator
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy
 
 LINE_TYPE_UNCHANGED = 0 # Status unchanged from last line. Usually a continued message.
 LINE_TYPE_DATE      = 1 # Line with a single date (new day). Can be ignored.
@@ -30,6 +35,9 @@ class Message:
     def add_line(self, text):
         self.text = self.text + "\n" + text
 
+    def count_words(self):
+        return len(re.findall(r'\b\w+\b', self.text))
+
     def __str__(self):
         return "[%s] %s: %s" % (self.dt, self.sender.name, self.text)
 
@@ -42,7 +50,7 @@ class Sender:
 
     def count_message(self, message):
         self.count = self.count + Counter(messages=1,
-                             words=len(re.findall(r'\b\w+\b', message.text)))
+                                          words=message.count_words())
 
 class MessageExportAnalyer:
     messages = []
@@ -57,17 +65,19 @@ class MessageExportAnalyer:
         Parses every line on its own, but handles
         continued messages
         """
-        f = open(input_file_path)
+        try:
+            f = open(input_file_path)
+        except IOError:
+            print "Input file not found."
+            sys.exit(1)
+
         last_message = None
         for line in f:
             line = line.strip()
-            if line == "":
+            if not line:
                 continue
-            parsed_line = self.parse_line(line)
-            (t, dt, sender_name, text) = parsed_line
-            if t == LINE_TYPE_DATE:
-                continue
-            elif t == LINE_TYPE_UNCHANGED and last_message:
+            (t, dt, sender_name, text) = self.parse_line(line)
+            if t == LINE_TYPE_UNCHANGED and last_message:
                 last_message.add_line(text)
             elif t == LINE_TYPE_MESSAGE:
                 s = None
@@ -124,54 +134,97 @@ class MessageExportAnalyer:
         """
         Prints some simple stats
         """
+        print "\nCounts per sender:"
         for (name, sender) in self.senders.iteritems():
-            print "%s: %d messages, %d words" % (sender.name, sender.count["messages"], sender.count["words"])
+            print "%s:  %d messages, %d words, %.2f words/message" % (sender.name,
+                        sender.count["messages"], sender.count["words"],
+                        sender.count["words"]/sender.count["messages"])
 
-    def messages_per_period(self, period):
+        data = self.count_per_period('messages', 'day')
+        print "\nMost active days:"
+
+        sorted_data = sorted(data, key=operator.itemgetter(1), reverse=True)
+        for date, count in sorted_data[:10]:
+            print "%s: %d messages" % (date, count)
+
+    def count_per_period(self, counter, period):
         """
-        Returns a tuple of (list of dates, list of number of messages on the corresponding dates)
+        Returns a list of (date, number of messages) tuples
         """
         dates = []
         counts = Counter()
+
+        # Count messages
         for message in self.messages:
             date = message.dt.date()
+
+            # Depending on requested period, adjust date
             if period == 'day':
                 pass
             elif period == 'week':
-                date = date + datetime.timedelta(days=-date.isoweekday())
+                date = date + datetime.timedelta(days=1-date.isoweekday())
             elif period == 'month':
-                date = date + datetime.timedelta(days=-date.day)
-            dates.append(date)
-            counts = counts + Counter({date: 1})
+                date = date + datetime.timedelta(days=1-date.day)
+            if not date in dates:
+                dates.append(date)
 
-        values = []
+            # Determine what to count
+            cnt = 0
+            if counter == 'messages':
+                cnt = 1
+            elif counter == 'words':
+                cnt = message.count_words()
+
+            counts = counts + Counter({date: cnt})
+
+        values_out = []
+        last_date = None
+
+        # Timedelta to account for missing dates
+        td = None
+        if period == 'day':
+            td = datetime.timedelta(days=1)
+        elif period == 'week':
+            td = datetime.timedelta(days=7)
+        elif period == 'month':
+            td = relativedelta(months=1)
+
         for date in dates:
-            values.append( counts[date] )
+            # Fill in missing (count=0) dates
+            if last_date:
+                while (last_date+td) < date:
+                    fill_date = last_date + td
+                    values_out.append((fill_date, 0))
+                    last_date = fill_date
+            values_out.append((date, counts[date]))
+            last_date = date
 
-        return (dates, values)
+        return values_out
 
-    def plot(self):
+    def plot(self, counter, period):
         """
         Plots the message counts with matplotlib
         Built on example from http://matplotlib.org/1.3.1/examples/api/date_demo.html
         """
-        years     = mdates.YearLocator()
         months    = mdates.MonthLocator()
         weeks     = mdates.WeekdayLocator(byweekday=mdates.MO)
-        monthsFmt = mdates.DateFormatter('%m. %Y')
+        date_formatter = mdates.DateFormatter('%m. %Y')
+
         fig, ax = plt.subplots()
 
-        (dates, values) = self.messages_per_period('month')
-        ax.bar(dates, values, label="Messages per month")
+        data = self.count_per_period(counter, period)
+        (dates, values) = zip(*data)
+        ax.plot(dates, values, label=("Messages per %s" % (period)))
 
-        (dates, values) = self.messages_per_period('week')
-        ax.plot(dates, values, label="Messages per week")
+        # Plot trend
+        x = range(0, len(dates))
+        fit = numpy.polyfit(x, values, 1)
+        fit_fn = numpy.poly1d(fit)
+        ax.plot(dates, fit_fn(x), '--k')
 
-        (dates, values) = self.messages_per_period('day')
-        ax.plot(dates, values, label="Messages per day")
-
+        # Format for axis
         ax.xaxis.set_major_locator(months)
-        ax.xaxis.set_major_formatter(monthsFmt)
+        ax.xaxis.set_major_formatter(date_formatter)
         ax.xaxis.set_minor_locator(weeks)
 
         ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
@@ -181,7 +234,6 @@ class MessageExportAnalyer:
         legend = ax.legend(loc='upper center')
 
         fig.autofmt_xdate()
-
         plt.show()
 
     def output(self):
@@ -189,18 +241,39 @@ class MessageExportAnalyer:
             print "[%s] [%s] %s" % (message.dt, message.sender, message.text)
 
 if __name__ == "__main__":
+
+    possible_periods = ['day', 'week', 'month']
+    possible_actions = ['data', 'stat', 'plot']
+
     if (len(sys.argv) > 2):
-        action = sys.argv[1];
-        input_file_path = sys.argv[2];
+        input_file_path = sys.argv[1];
+
+        action = sys.argv[2];
+        if not action in possible_actions:
+            print "Invalid action. Use one of %s" % (", ".join(possible_actions))
+            sys.exit(1)
+
+        period = 'day'
+        if len(sys.argv) > 3:
+            period = sys.argv[3];
+        if not period in possible_periods:
+            print "Invalid period. Use one of %s" % (", ".join(possible_periods))
+            sys.exit(1)
+
         print "Parsing file..."
         analyzer = MessageExportAnalyer(input_file_path)
         if action == "stat":
             analyzer.stats()
         elif action == "plot":
             print "Generating plots..."
-            analyzer.plot()
+            analyzer.plot('messages', period)
+        elif action == "data":
+            data = analyzer.count_per_period('messages', period)
+            for date, count in data:
+                print "%s: %d" % (date, count)
         sys.exit(0)
     else:
-        print "Usage: %s action input-file.txt" % __file__
-        print "  Actions: stat, plot"
+        print "Usage: %s input-file.txt action [period]" % __file__
+        print "  Actions: %s" % (", ".join(possible_actions))
+        print "  Periods: %s" % (", ".join(possible_periods))
         sys.exit(1)
